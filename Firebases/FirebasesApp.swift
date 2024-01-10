3c40ae9ad53655e14037011135ee67c0e21a197c
YorgiAlejandro <yorgidelriomarquez@gmail.com>
2024-01-10 11:48:12 -0500

    Initial Commit

---
+++ Firebases/FirebasesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebasesApp.swift
+//  Firebases
+//
+//  Created by BeMillionaire on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebasesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
